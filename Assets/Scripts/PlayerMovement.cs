13582e084600abd02943bb07227cedb14a511caa
RivadonS <rivadon.sung@bumail.net>
2025-11-20 15:20:19 +0700

    Start Script

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+    public Rigidbody2D rb;
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        rb.linearVelocity = new Vector2 (horizontal, vertical);
+    }
+}
