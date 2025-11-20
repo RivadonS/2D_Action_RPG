46758d4371d32537e97a5edffd9ee55104107731
RivadonS <rivadon.sung@bumail.net>
2025-11-20 15:58:53 +0700

    Top-down Movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,13 +5,12 @@ public class PlayerMovement : MonoBehaviour
     public float speed = 5f;
     public Rigidbody2D rb;
 
-    
-
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
-        rb.linearVelocity = new Vector2 (horizontal, vertical);
+
+        rb.linearVelocity = new Vector2 (horizontal, vertical) * speed;
     }
 }
