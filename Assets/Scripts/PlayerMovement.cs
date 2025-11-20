700727aaef77ca0220dfce29f1b5973e84d2c770
RivadonS <rivadon.sung@bumail.net>
2025-11-20 17:44:25 +0700

    Flip

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,14 +3,32 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 5f;
-    public Rigidbody2D rb;
+    public int facingDirection = 1;
 
-    // Update is called once per frame
+    public Rigidbody2D rb;
+    public Animator anim;
+
+    // Fixed Update is called 50x Frame
     void FixedUpdate()
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
+        if (horizontal > 0 && transform.localScale.x < 0 || 
+            horizontal < 0 && transform.localScale.x > 0)
+        {
+            Flip();
+        }
+
+        anim.SetFloat("horizontal", Mathf.Abs(horizontal));
+        anim.SetFloat("vertical", Mathf.Abs(vertical));
+
         rb.linearVelocity = new Vector2 (horizontal, vertical) * speed;
+    }
+
+    void Flip()
+    {
+        facingDirection *= -1;
+        transform.localScale = new Vector3 (transform.localScale.x * -1, transform.localScale.y, transform.localScale.z)
     }
 }
